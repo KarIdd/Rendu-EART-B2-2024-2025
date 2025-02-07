d08feddf1bea546d8a6d4760da3f39c860a839a4
Anthonin de La Gorce Arfaoui <anthonin.delagorce@gmail.com>
2025-02-07 17:10:48 +0100

    Inté lights

--- Assets/_General/Code/Scripts/PlayerController.cs
+++ Assets/_General/Code/Scripts/PlayerController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
-public class FPSController : MonoBehaviour
+public class FPSControlleer : MonoBehaviour
 {
     public Camera playerCamera;
     public float walkSpeed = 6f;
